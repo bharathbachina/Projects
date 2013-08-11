c76e0bba65611ecd9d33aeb8a6e1e8650530b426
bharathbachina <itsforyouandmera@gmail.com>
2013-08-11 12:17:36 +0530

    added comment

--- RouteSync/src/com/route/routesync/GPSListener.java
+++ RouteSync/src/com/route/routesync/GPSListener.java
@@ -2,6 +2,11 @@ package com.route.routesync;
 
 public interface GPSListener {
 	
+	/**
+	 * This is used to send latitude and longitude
+	 * @param latitude
+	 * @param longitude
+	 */
 	public void getLocation(double latitude,double longitude);
 
 }
